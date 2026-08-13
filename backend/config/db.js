9e0bed59754abf532edee966dc675aad89346d9b
Samiksha Jadhav <samikshajadhav260@gmail.com>
2026-08-13 17:13:19 +0530

    Configure database for deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Sam@1234",
-    database: "store_rating_db"
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_NAME || "store_rating_db",
+    port: process.env.DB_PORT || 3306
 });
 
 db.connect((err) => {
